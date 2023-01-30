948de736da647da947e0c3f93a51515327996865
Damien Irving <irving.damien@gmail.com>
2023-01-31 10:06:43 +1100

    Add MIROC6

---
+++ config/config_MIROC6.mk
@@ -0,0 +1,13 @@
+
+MODEL=MIROC6
+EXPERIMENT=dcppA-hindcast
+BASE_PERIOD=1960-01-01 2018-12-31
+BASE_PERIOD_TEXT=196011-201811
+TIME_PERIOD_TEXT=196011-201811
+MIN_LEAD=1
+MODEL_IO_OPTIONS=--n_ensemble_files 10 --n_time_files 2
+MODEL_NINO_OPTIONS=--n_ensemble_files 10 --lon_bnds 190 240 --lat_dim latitude --lon_dim longitude --agg_y_dim y --agg_x_dim x --anomaly ${BASE_PERIOD} --anomaly_freq month
+
+
+
+
