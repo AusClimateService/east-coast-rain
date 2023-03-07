c6089de4949ecde64cd33c6fbe6c52c3c6fb30c8
Damien Irving <irving.damien@gmail.com>
2023-03-07 15:23:59 +1100

    Add CMCC

--- config/config_CMCC-CM2-SR5.mk
+++ config/config_CMCC-CM2-SR5.mk
@@ -6,5 +6,6 @@ MIN_LEAD=4
 BASE_PERIOD=1960-01-01 2019-12-31
 BASE_PERIOD_TEXT=196011-201911
 TIME_PERIOD_TEXT=196011-201911
+MODEL_NINO_OPTIONS=${MODEL_IO_OPTIONS} --lon_bnds 190 240 --lat_dim latitude --lon_dim longitude --agg_y_dim j --agg_x_dim i --anomaly ${BASE_PERIOD} --anomaly_freq month
 
 
