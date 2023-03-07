36f32209c1a65332a5f1e0d07ee4c8d57f69a291
Damien Irving <irving.damien@gmail.com>
2023-03-07 16:46:53 +1100

    Add Nino

--- config/config_EC-Earth3.mk
+++ config/config_EC-Earth3.mk
@@ -6,5 +6,6 @@ MIN_LEAD=0
 BASE_PERIOD=1960-01-01 2017-12-20
 BASE_PERIOD_TEXT=1960-2017
 TIME_PERIOD_TEXT=196011-201711
+MODEL_NINO_OPTIONS=${MODEL_IO_OPTIONS} --lon_bnds 190 240 --lat_dim latitude --lon_dim longitude --agg_y_dim j --agg_x_dim i --anomaly ${BASE_PERIOD} --anomaly_freq month
 
 
